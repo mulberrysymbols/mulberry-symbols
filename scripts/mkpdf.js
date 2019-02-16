8dee391c9cf3587739aecfbc6b89df2bf74e6511
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-02-16 23:51:10 +0000

    Code cleanup

--- scripts/mkpdf.js
+++ scripts/mkpdf.js
@@ -1,10 +1,19 @@
 (async function() {
   const puppeteer = require('puppeteer');
   const path = require('path');
-  const getContent = require('./mk');
+  const getContent = require('./mk-template');
 
-  const CATEGORIES_PDF_FILE_NAME = 'categories/categories.pdf';
+  const CATEGORIES_PDF_FILE_NAME = path.resolve(
+    __dirname,
+    '..',
+    'categories/categories.pdf',
+  );
   const TEMPLATE_PATH = path.resolve(__dirname, 'templates/pdf-template.html');
+
+  const DIRECTORY_NAME = 'categories';
+  if (!fs.existsSync(DIRECTORY_NAME)) {
+    fs.mkdirSync(DIRECTORY_NAME);
+  }
 
   const htmlContent = await getContent(TEMPLATE_PATH);
 
