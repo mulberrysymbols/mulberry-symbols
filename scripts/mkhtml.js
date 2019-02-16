32fcfc223414df8caf6d25f173859dc22b498ffa
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-02-16 23:39:53 +0000

    PDF and HTML can be generated seperately

--- scripts/mkhtml.js
+++ scripts/mkhtml.js
@@ -1,109 +1,23 @@
-const path = require('path');
-const handlebars = require('handlebars');
-const util = require('util');
-const fs = require('fs');
+(async function() {
+  const fs = require('fs');
+  const path = require('path');
+  const getContent = require('./mk');
+  const util = require('util');
 
-const NAME_INDEX = 1;
-const CATEGORY_INDEX = 3;
-const SVG_BASE_PATH = path.resolve(__dirname, '..', 'EN');
-const PDF_TEMPLATE_PATH = path.resolve(
-  __dirname,
-  'templates/pdf-template.html',
-);
-const CONTENT_TEMPLATE_PATH = path.resolve(
-  __dirname,
-  'templates/content-template.html',
-);
-const WEBPAGE_TEMPLATE_PATH = path.resolve(
-  __dirname,
-  'templates/webpage-template.html',
-);
-const asyncReadFile = util.promisify(fs.readFile);
-const asyncWriteFile = util.promisify(fs.writeFile);
-const asyncMkDir = util.promisify(fs.mkdir);
-const CATEGORIES_HTML_FILE_NAME = 'categories/categories.html';
+  const CATEGORIES_HTML_FILE_NAME = 'categories/categories.html';
+  const WEBPAGE_TEMPLATE_PATH = path.resolve(
+    __dirname,
+    'templates/webpage-template.html',
+  );
+  const DIRECTORY_NAME = 'categories';
 
-const slugify = (unslugged) => unslugged.toLowerCase().replace(/\s/g, '-');
+  const asyncWriteFile = util.promisify(fs.writeFile);
 
-/**
- * Convert an icon name to a human-readable string
- *
- * @param {string} originalString
- */
-function convertToDisplayString(originalString) {
-  return originalString.replace(/_/g, ' ');
-}
+  const htmlContent = await getContent(WEBPAGE_TEMPLATE_PATH);
 
-/**
- * Generate HTML content of the PDF report
- *
- * @param {*} data
- */
-async function asyncGenerateHtmlContent(data) {
-  const categories = {};
-
-  for (const row of data) {
-    let category = row[CATEGORY_INDEX];
-
-    if (category === '') {
-      category = 'Uncatagorized';
-    }
-
-    const name = row[NAME_INDEX];
-    const iconContent = await asyncReadFile(
-      path.resolve(SVG_BASE_PATH, `${name}.svg`),
-      'utf8',
-    );
-
-    const categoryTemplate = {
-      name: convertToDisplayString(name),
-      content: encodeURIComponent(iconContent),
-    };
-
-    if (categories[category] === undefined) {
-      categories[category] = [categoryTemplate];
-    } else {
-      categories[category].push(categoryTemplate);
-    }
+  if (!fs.existsSync(DIRECTORY_NAME)) {
+    fs.mkdirSync(DIRECTORY_NAME);
   }
 
-  const sortedCategories = {};
-
-  for (const category of Object.keys(categories).sort()) {
-    sortedCategories[category] = categories[category];
-  }
-
-  const contentFile = await asyncReadFile(CONTENT_TEMPLATE_PATH, 'utf8');
-  const pdfFile = await asyncReadFile(PDF_TEMPLATE_PATH, 'utf8');
-  const htmlFile = await asyncReadFile(WEBPAGE_TEMPLATE_PATH, 'utf8');
-
-  const contentTemplate = handlebars.compile(contentFile);
-  const pdfTemplate = handlebars.compile(pdfFile);
-  const htmlTemplate = handlebars.compile(htmlFile);
-
-  const anchoredCategories = {};
-  Object.keys(sortedCategories)
-    .sort()
-    .forEach((currentKey) => {
-      anchoredCategories[currentKey] = {
-        anchor: slugify(currentKey),
-        symbols: sortedCategories[currentKey],
-      };
-    });
-
-  const content = contentTemplate({
-    categories: anchoredCategories,
-  });
-
-  const html = htmlTemplate({ content });
-  const pdf = pdfTemplate({
-    content,
-    version: require('../package.json').version,
-  });
-
-  await asyncWriteFile(CATEGORIES_HTML_FILE_NAME, html);
-
-  return pdf;
-}
-
-module.exports = asyncGenerateHtmlContent;
+  await asyncWriteFile(CATEGORIES_HTML_FILE_NAME, htmlContent);
+})();
---
+++ scripts/parse-symbols.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const parse = require('csv-parse');
+const asyncReadFile = util.promisify(fs.readFile);
+
+const NAME_INDEX = 1;
+const CATEGORY_INDEX = 3;
+const SVG_BASE_PATH = path.resolve(__dirname, '..', 'EN');
+
+const slugify = (unslugged) => unslugged.toLowerCase().replace(/\s/g, '-');
+
+async function asyncParseCSV(pathToCSV) {
+  const CSV_PATH = pathToCSV || path.resolve(__dirname, '../symbol-info.csv');
+  const csvContents = await asyncReadFile(CSV_PATH, 'utf8');
+
+  return new Promise((resolve, reject) => {
+    parse(csvContents, { from: 2, cast: true }, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
+    });
+  });
+}
+
+/**
+ * Convert an icon name to a human-readable string
+ *
+ * @param {string} originalString
+ */
+function convertToDisplayString(originalString) {
+  return originalString.replace(/_/g, ' ');
+}
+
+module.exports = async function() {
+  const categories = {};
+  const data = await asyncParseCSV();
+
+  for (const row of data) {
+    let category = row[CATEGORY_INDEX];
+
+    if (category === '') {
+      category = 'Uncatagorized';
+    }
+
+    const name = row[NAME_INDEX];
+    const iconContent = await asyncReadFile(
+      path.resolve(SVG_BASE_PATH, `${name}.svg`),
+      'utf8',
+    );
+
+    const categoryTemplate = {
+      name: convertToDisplayString(name),
+      content: encodeURIComponent(iconContent),
+    };
+
+    if (categories[category] === undefined) {
+      categories[category] = [categoryTemplate];
+    } else {
+      categories[category].push(categoryTemplate);
+    }
+  }
+
+  const sortedCategories = {};
+
+  for (const category of Object.keys(categories).sort()) {
+    sortedCategories[category] = categories[category];
+  }
+
+  const anchoredCategories = {};
+  Object.keys(sortedCategories)
+    .sort()
+    .forEach((currentKey) => {
+      anchoredCategories[currentKey] = {
+        anchor: slugify(currentKey),
+        symbols: sortedCategories[currentKey],
+      };
+    });
+
+  return anchoredCategories;
+};
