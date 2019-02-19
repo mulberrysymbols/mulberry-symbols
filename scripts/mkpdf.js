ecb354166d1d544544c2a3c748b6c57c881ad1c5
Steve Lee <steve@opendirective.com>
2019-02-19 11:02:54 +0000

    Add missing fs dependency

--- scripts/mkpdf.js
+++ scripts/mkpdf.js
@@ -1,5 +1,6 @@
-(async function() {
+(async function () {
   const puppeteer = require('puppeteer');
+  const fs = require('fs');
   const path = require('path');
   const getContent = require('./mk-template');
 
